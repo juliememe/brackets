bb1845c336412119fb41e8e28929f20c0644ff70
julie_meme <julie_meme@mail.ru>
2020-10-19 15:47:45 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ while(str){
+     let counter=0;
+for (const j of bracketsConfig) {
+    
+    let newConfig = j[0] + j[1];
+
+if(str.includes(newConfig)){
+    str= str.replace(newConfig, '');
+    counter++;
+}
+}
+if(counter == 0)
+    break;
+
+ }
+return str == '' ? true : false;
+
 }
